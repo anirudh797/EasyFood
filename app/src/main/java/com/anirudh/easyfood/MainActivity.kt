93a55249e844fd88d097141ca61f459e86c9b3b7
anirudhkaptiyal <anirudh.kaptiyal@telekom-digital.com>
2024-02-02 10:55:26 +0530

    Initial commit

---
+++ app/src/main/java/com/anirudh/easyfood/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.anirudh.easyfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = Navigation.findNavController(this,R.id.nav_host_fragment_container)
+
+        NavigationUI.setupWithNavController(bottomNav,navController)
+    }
+}
